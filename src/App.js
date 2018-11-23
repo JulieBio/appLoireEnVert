e849e6eec5ca6cd5c60af2d3739ec0d79fa9062a
JulieBio <juliebio69@gmail.com>
2018-11-23 12:43:25 +0100

    event css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 //import Result from './containers/Result';
 //import { Provider } from 'react-redux';
 //import { createStore, applyMiddleware } from 'redux';
 //import thunk from 'redux-thunk'
 //import reducers from './reducers';
 import Head from './components/head';
-
-
+import EventList from './components/EventList';
 
 class App extends Component {
   render() {
     return (
         <div>
           <Head/>
+          <EventList/>
         </div>
     );
   }
