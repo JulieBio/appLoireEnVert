6ed4afda9ec10e7cae644fee6ecb5f0c98083b34
Marion Koo sin lin <marionksl@gmail.com>
2018-11-28 15:46:17 +0100

    eventDetail

--- front/src/components/EventDetails.jsx
+++ front/src/components/EventDetails.jsx
@@ -1,24 +1,71 @@
-/*import React, { Component } from "react";
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import './Event.css';
+import { Container, Row} from 'reactstrap';
 import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  Button
+Card,
+CardImg,
+CardBody,
+CardTitle,
+CardSubtitle,
+CardText
 } from "reactstrap";
 
-class EventDetails extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.type}
-      </div>
-    );
-  }
-}
+const EventDetails = ({event}) => (
+    <Container>
+        <div
+            className="cardAccueil"
+            onClick={() => this.activitySelected(event)}>
 
-export default EventDetails;*/
+            <Card body outline>
+            <CardImg top cardImage src={event.image} alt="Card image cap" classname="images" />
+              <CardBody classname="bodyCard">
+                <Row classname="description">
+                  {/*type*/}
+                    <CardTitle className="type">{event.type}</CardTitle>
+                </Row>
+                <Row>
+                  {/*name*/}
+                  <CardSubtitle className="nom">{event.name}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                    {/*where*/}
+                  <CardSubtitle className="where">{event.where}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                    {/*Place*/}
+                  <CardSubtitle className="place">{event.place}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                      {/*city*/}
+                  <CardSubtitle className="city">{event.city}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                  {/*date*/}
+                <CardSubtitle className="quand">{event.date}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                  {/*time*/}
+                  <CardSubtitle className="heure">{event.time}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                  {/*free*/}
+                  <CardSubtitle className="free">{event.free}</CardSubtitle>
+                </Row>
+                <br/>
+                <Row>
+                  <CardText className="description">{event.description}</CardText>
+                </Row>
+                <br/>
+              </CardBody>
+            </Card>
+        </div>
+     </Container>
+
+);
+export default EventDetails;
