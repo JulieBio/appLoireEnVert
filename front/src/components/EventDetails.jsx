f787775fbb9a7037c668aaa6cac71b39186a6723
Marion Koo sin lin <marionksl@gmail.com>
2018-11-28 15:13:30 +0100

    .

--- front/src/components/EventDetails.jsx
+++ front/src/components/EventDetails.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+/*import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Event.css';
 import {
@@ -21,4 +21,4 @@ class EventDetails extends Component {
   }
 }
 
-export default EventDetails;
+export default EventDetails;*/
