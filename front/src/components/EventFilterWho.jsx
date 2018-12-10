543741f22fdfbc76b957fac541749423bf2ee1cc
Monica Vargas <mo.va.sanchez@gmail.com>
2018-12-10 10:26:17 +0100

    modification

--- front/src/components/EventFilterWho.jsx
+++ front/src/components/EventFilterWho.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Button } from "reactstrap";
 import {connect} from "react-redux";
 import { updateFilter } from "../actions/index";
-import { withRouter } from 'react-router-dom';
-// import EventList from "../containers/EventList";
+
+
 
 
 class EventFilterWhere extends Component {
@@ -12,49 +11,16 @@ class EventFilterWhere extends Component {
     super(props);
     this.state = {
       eventsFiltred: null
-      //       // filterWho: null, à utiliser après
+ 
     };
-    //this.onClick = this.onClick.bind(this);
+
     this.id = this.props.match.params.id;
-    // this.who = this.props.match.params.who;
+
   }
 
   componentWillMount() {
-    //Récupération de resultats filtrés via axios
-    // axios.get(`/event?where=stephanois-pilat`).then(result => {
-    //   console.log("axios",result.data)
-    //   // Une fois les données récupérées, on va mettre à jour notre state avec les nouvelles données
-    //   this.setState({
-    //     eventsFiltred: result.data,
 
-    //   });
-    // });
-    // this.props.functionCallDispatch();
   }
-
-  /*routeChangeForez = () =>{
-    console.log('was click')
-    let path = "event/forez"
-    this.props.history.push(path);
-    }
-  routeChangeRoannais = () => {
-    let path = "event/roannais"
-    this.props.history.push(path);
-    }
- routeChangeStephanois = () => {
-    console.log('was click')
-    let path = "/event/stephanois-pilat"
-    this.props.history.push(path)
-    }*/
-
-  /*onClick(e) {
-    this.setState({
-    [e.target.eventsFiltred]: e.target.value, 
-    
-    
-    });
-    console.log(3);
-    }*/
 
   eventsFiltred = who => {
     this.props.updateFilter({ who: who });
