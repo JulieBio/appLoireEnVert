980aefd843e2d820256fb21c81c00ba453ef8906
Monica Vargas <mo.va.sanchez@gmail.com>
2018-12-13 11:24:36 +0100

    ok

--- front/src/components/EventFilterWhere.jsx
+++ front/src/components/EventFilterWhere.jsx
@@ -18,7 +18,7 @@ class EventFilterWhere extends Component {
   }
 
   componentWillMount() {}
-  
+
   //Fonction qui prend "where" et qui va actualiser la liste filtrée
   // "where" et qui va pusher le result en tant que historique à la page "who"
   //On trouve updateFilter en tant que fichier action.
@@ -29,47 +29,47 @@ class EventFilterWhere extends Component {
 
   render() {
     return (
-        <Container className="containerOu">
-          <h1 className="titleOu">Où aller?</h1>
+      <Container className="containerOu">
+        <h1 className="titleOu">Où aller?</h1>
 
-          <Button
-            className="buttonStephanois"
-            onClick={() => this.eventsFiltred("Stéphanois-Pilat")} //Rappelle de la fonction eventsFiltred
-            color="secondary"
-            size="lg"
-            block
-          >
-            <p>
-              <img src={stephanois} alt="mapStéphanois" />
-            </p>
-            <p className="titleStéphanois">STEPHANOIS-PILAT</p>
-          </Button>
-          <Button
-            className="buttonForez"
-            onClick={() => this.eventsFiltred("Forez")}
-            color="secondary"
-            size="lg"
-            block
-          >
-            <p>
-              <img src={forez} alt="mapForez" />
-            </p>
-            <p className="titleForez">FOREZ</p>
-          </Button>
-          <Button
-            className="buttonRoannais"
-            onClick={() => this.eventsFiltred("Roannais")}
-            color="secondary"
-            size="lg"
-            block
-          >
-            <p>
-              <img src={roannais} alt="mapRoannais" />
-            </p>
-            <p className="titleRoannais">ROANNAIS</p>
-          </Button>
-        </Container>
-      );
+        <Button
+          className="buttonStephanois"
+          onClick={() => this.eventsFiltred("Stéphanois-Pilat")} //Rappelle de la fonction eventsFiltred
+          color="secondary"
+          size="lg"
+          block
+        >
+          <p>
+            <img src={stephanois} alt="mapStéphanois" />
+          </p>
+          <p className="titleStéphanois">STEPHANOIS-PILAT</p>
+        </Button>
+        <Button
+          className="buttonForez"
+          onClick={() => this.eventsFiltred("Forez")}
+          color="secondary"
+          size="lg"
+          block
+        >
+          <p>
+            <img src={forez} alt="mapForez" />
+          </p>
+          <p className="titleForez">FOREZ</p>
+        </Button>
+        <Button
+          className="buttonRoannais"
+          onClick={() => this.eventsFiltred("Roannais")}
+          color="secondary"
+          size="lg"
+          block
+        >
+          <p>
+            <img src={roannais} alt="mapRoannais" />
+          </p>
+          <p className="titleRoannais">ROANNAIS</p>
+        </Button>
+      </Container>
+    );
   }
 }
 
