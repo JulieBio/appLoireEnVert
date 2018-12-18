f772d3b67cda4597b7b6c676e315c5516b18db66
JulieBio <juliebio69@gmail.com>
2018-12-18 12:03:54 +0100

    misa a jour navbar

--- back/src/conf.js
+++ back/src/conf.js
@@ -1,8 +1,9 @@
 const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
+port: '8889',
 user :  'root', // le nom d'utilisateur
-password :  'stmysql', // le mot de passe
+password :  'root', // le mot de passe
 database :  'Loire_en_Vert', // le nom de la base de données
 });
 module.exports = connection;
