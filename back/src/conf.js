a17e86dbdb74e937a9a29b5d259f355abb8a9f64
JulieBio <juliebio69@gmail.com>
2018-12-19 11:22:48 +0100

    maj polices et card

--- back/src/conf.js
+++ back/src/conf.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host: 'localhost', // adresse du serveur
   port: '8889',
   user: 'root', // le nom d'utilisateur
-  password: '******', // le mot de passe
+  password: 'root', // le mot de passe
   database: 'Loire_en_Vert', // le nom de la base de données
 });
 module.exports = connection;
