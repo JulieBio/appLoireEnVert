e9c538840ad4b4032e4ed92633f21a10db29fd6d
Monica Vargas <mo.va.sanchez@gmail.com>
2018-12-19 14:09:50 +0100

    ok

--- back/src/conf.js
+++ back/src/conf.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: 'localhost', // adresse du serveur
-  port: '8889',
+  // port: '8889',
   user: 'root', // le nom d'utilisateur
-  password: '******', // le mot de passe
+  // password: '******', // le mot de passe
   database: 'Loire_en_Vert', // le nom de la base de données
 });
 module.exports = connection;
