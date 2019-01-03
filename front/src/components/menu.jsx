865bd78eb456530bb4505b76c8c1a296594a55af
Nadim Mabrouk <mabrouknadim2@gmail.com>
2019-01-03 15:03:00 +0100

    modification menu burger

--- front/src/components/menu.jsx
+++ front/src/components/menu.jsx
@@ -1,18 +1,4 @@
 import React, { Component } from "react";
-import close from "../assets/close.png";
-
-//Julie pour la croix "close"
-const styleClose = {
-  goBack: {
-    background: `url(${close})`,
-    opacity: "0.5",
-    backgroundSize: "55%",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    height: "6vh",
-    width: "6vh",
-  }
-};
 
 // Julie : liste des links du menu burger
 class Menu extends Component {
@@ -25,34 +11,17 @@ class Menu extends Component {
   render() {
     return (
       <div>
-        <div className="menuClose"
-          style={{
-            // position: "fixed",
-            backgroundColor: "rgb(240,240,240,0.5)",
-            height: "6vh",
-            width: "6vh",
-            borderRadius: "25px",
-            zIndex: "2"
-          }}>
-
-          <div className="closeCross" style={styleClose.goBack} onClick={this.goBack} />
-        </div>
-        
         <div className="menuBurger">
           <div className="menuB"><a href="https://loireenvert.fr/">Accès au site internet</a></div>
-          <div className="menuB"><a href="/event">Paramètres</a></div>
-          <div className="menuB"><a href="/event">Mon compte</a></div>
-          <div className="menuB"><a href="/event">Newsletter</a></div>
+          <div className="menuB"><a href="https://loireenvert.fr/newsletter/">Newsletter</a></div>
           <div className="menuB"><a href="/quiSommesNous">Qui sommes-nous ?</a></div>
           <div className="menuB"><a href="/partenaires">Partenaires</a></div>
           <div className="menuB"><a href="https://loireenvert.fr/contact/">Contact</a></div>
-          <div className="menuB"><a href="/event">Vos données</a></div>
         </div>
         <div className="espace"> </div>
       </div >
     )
   }
 };
-
 
 export default Menu;
