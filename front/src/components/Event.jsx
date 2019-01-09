c6ba731d8d6bd2e58ca7d1a090e07b63c5c2a92f
Monica Vargas <mo.va.sanchez@gmail.com>
2019-01-09 15:03:44 +0100

    problème affichage texte ok

--- front/src/components/Event.jsx
+++ front/src/components/Event.jsx
@@ -5,7 +5,7 @@ import backgroundEuro from "../assets/euro.png";
 import { Card, CardImg, CardBody, CardSubtitle } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import moment from "moment";
-import LinesEllipsis from 'react-lines-ellipsis'
+import LinesEllipsis from "react-lines-ellipsis";
 import "./eventList.css";
 
 const styleEuro = {
@@ -25,56 +25,50 @@ const styleEuro = {
 const idLocale = require("moment/locale/fr");
 moment.locale("fr", idLocale);
 
-
 const Event = ({ event }) => (
-
-  <Container>
-    <div className="cardAccueil">
-      <NavLink to={`/event/${event.id}`} className="lienAccueil">
-        <Card>
-          <CardImg
-            top
-            src={event.image}
-            alt="Card image cap"
-            className="cardeImage"
+  <div className="cardAccueil">
+    <NavLink to={`/event/${event.id}`} className="lienAccueil">
+      <Card>
+        <CardImg
+          top
+          src={event.image}
+          alt="Card image cap"
+          className="cardeImage"
           /*  width="100%" */
+        />
+        <CardBody className="cardContent">
+          <LinesEllipsis
+            className="eventTitle"
+            text={event.name}
+            maxLine="2"
+            basedOn="letters"
           />
-          <CardBody className="cardContent">
-            <LinesEllipsis
-              className="eventTitle"
-              text={event.name}
-              maxLine="2"
-              basedOn='letters'
-            />
-            <CardSubtitle className="itemEvent">
-              <p className="typeEvent">{event.type} </p>
-            </CardSubtitle>
-            <CardSubtitle className="itemEvent">
-              <p>
-                {moment(event.event_date_start).format("ll")} -{" "}
-                {moment(event.event_date_finish).format("ll")}
-                <br />
-                {event.event_time}{" "}
-              </p>
-            </CardSubtitle>
-            <CardSubtitle className="itemEvent">
-              {event.event_where}
-            </CardSubtitle>
-            <CardSubtitle>
-              <div className="cardIcones">
-                {event.free === "true" ? (
-                  <h1> </h1>
-                ) : (
-                    <div style={styleEuro.euro} />
-                  )}
-                <div className={event.type} />
-              </div>
-            </CardSubtitle>
-          </CardBody>
-        </Card>
-      </NavLink>
-    </div>
-  </Container>
+          <CardSubtitle className="itemEvent">
+            <p className="typeEvent">{event.type} </p>
+          </CardSubtitle>
+          <CardSubtitle className="itemEvent">
+            <p>
+              {moment(event.event_date_start).format("ll")} -{" "}
+              {moment(event.event_date_finish).format("ll")}
+              <br />
+              {event.event_time}{" "}
+            </p>
+          </CardSubtitle>
+          <CardSubtitle className="itemEvent">{event.event_where}</CardSubtitle>
+          <CardSubtitle>
+            <div className="cardIcones">
+              {event.free === "true" ? (
+                <h1> </h1>
+              ) : (
+                <div style={styleEuro.euro} />
+              )}
+              <div className={event.type} />
+            </div>
+          </CardSubtitle>
+        </CardBody>
+      </Card>
+    </NavLink>
+  </div>
 );
 
 export default Event;
