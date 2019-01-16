119da006dbea16afd909972e44beaf30fa69ea39
Elizaveta Patrusheva <elizaveta.np@gmail.com>
2019-01-16 14:49:26 +0100

    nothing

--- back/src/conf.js
+++ back/src/conf.js
@@ -1,8 +1,7 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost", // adresse du serveur
-  user: "root", // le nom d'utilisateur
-  password: "*****", // le mot de passe
+  user: "*****", // le mot de passe
   database: "Loire_en_Vert" // le nom de la base de données
 });
 module.exports = connection;
