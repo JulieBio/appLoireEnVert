73307f1dc9579707f85fc4ebc14d60cf0878ade0
Anaele Salvatore <anaele.salvatore@gmail.com>
2019-01-16 16:54:12 +0100

    ajout reducer pour mise à jour filtre quand bouttons + ajout autorisation map mais ne fonctionne pas pour le moment

---
+++ front/src/reducers/button-dates.jsx
@@ -0,0 +1,18 @@
+const activeButton = (state = {
+    disabled: "1"
+}, action) => {
+    switch (action.type) {
+
+
+        case 'UPDATE_BUTTON':
+        console.log("fezfez", action.number)
+            return {
+                disabled: action.number
+            };
+        default:
+            return state
+
+    }
+}
+
+export default activeButton;
