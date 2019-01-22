dcd49a0340659226b0220d8a3415ca6986d09324
nadim <nadimabrouk@hotmail.fr>
2019-01-22 10:50:17 +0100

    recupération nouvelle dev

--- back/src/conf.js
+++ back/src/conf.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost", // adresse du serveur
-  user: "*****", // le mot de passe
+  user: "Safsoufa2016!", // le mot de passe
   database: "Loire_en_Vert" // le nom de la base de données
 });
 module.exports = connection;
